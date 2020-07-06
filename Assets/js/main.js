58cb19aa80d00395b5529a9a95d3fa9cca0b0790
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-07-06 20:00:32 +0100

    finished first setup

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -5,3 +5,15 @@ getElement(".open").addEventListener("click", () => {
 getElement(".close").addEventListener("click", () => {
     getElement("nav").classList.remove('active')
 })
+
+const getAllElement = (el) => document.querySelectorAll(el)
+
+const implementedIMG = getAllElement(".set-bg");
+
+//LOOP THROUGH ALL ITEM AND SET BACKGROUND
+
+implementedIMG.forEach(el =>{
+    const bg = el.dataset['setbg'];
+    console.log(bg)
+    el.style.backgroundImage=`url(${bg})`;
+})
